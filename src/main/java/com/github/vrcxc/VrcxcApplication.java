d2a4c6b86c51c0ec969f81ee0ad3caeeacf963e7
Yuki_Sakura <caiyucong12@gmial.com>
2025-06-01 17:22:30 +0800

    :tada: first commit

---
+++ src/main/java/com/github/vrcxc/VrcxcApplication.java
@@ -0,0 +1,13 @@
+package com.github.vrcxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrcxcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VrcxcApplication.class, args);
+    }
+
+}
